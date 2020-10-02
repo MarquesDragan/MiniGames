e56898192696e4f0c02e4e474a60f9d098b4e71b
MarquesDragan <47039402+MarquesDragan@users.noreply.github.com>
2020-10-02 16:25:55 -0500

    centering

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,15 @@
 // Minigame 1
 
-function ageInDays(){
-let birthYear = prompt("What Year Where You Born?");
+function numberOfDays(){
+    let birthYear = prompt("What Year Where You Born?");
+    let ageInDays = (2020 - birthYear) * 365;
+    let h1 = document.createElement('h1');
+    let answer = document.createTextNode('You Are ' + ageInDays + ' Days Old.');
+    h1.setAttribute('id', 'numberOfDays');
+    h1.appendChild(answer);
+    document.getElementById('flex-box-result').appendChild(h1);    
+}
+
+function reset() {
+    document.getElementById('numberOfDays').remove();
 }
